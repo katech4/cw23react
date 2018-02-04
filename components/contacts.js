1109e475bdf00bdf7a67d7688350c8a0efa494d0
Katarzyna Chmielewska <Katarzyna Chmielewska>
2018-02-04 21:52:57 +0100

    react

--- components/contacts.js
+++ components/contacts.js
@@ -35,3 +35,19 @@ var App = React.createClass({
     );
   }
 });
+
+var Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+    var contacts = this.props.items.map(function(contact) {
+        return React.createElement(Contact, {item: contact, key: contact.id});
+    });
+
+    return (
+      React.createElement('ul', {className: 'contactsList'}, contacts)
+    );
+  }
+});
